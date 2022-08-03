1c3c120e39494a507b87936f8a3079f3663a8600
nouhailamadrani <109972653+nouhailamadrani@users.noreply.github.com>
2022-08-03 13:08:32 +0200

    Add files via upload

--- calculatrice.c
+++ calculatrice.c
@@ -6,43 +6,16 @@ char operation , q;
 printf("entre le nombre A :  ");
 scanf("%d", &A );
 printf("entre l'operation : ");
-scanf("%c", &operation );
+scanf("%s", &operation );
+if(operation=='q'){
+    printf("arreter le programme !!");
+    exit(0);
+}
+else
+{
 printf("entre le nombre B :  ");
 scanf("%d", &B );
-
-switch (operation){
-
-case '+' :
-printf("%d" , A + B );
- break;
-case '-' :
-printf("%d" , A - B );
- break;
- case '/' :
- if( B !=0 )
-printf("%d" , A / B );
-else printf("impossi");
- break;
- case '*' :
-printf("%d" , A * B );
- break;
-default:
-printf("errerrr!");
-    break;
 }
-}
-
-#include <stdio.h>
-
-int main (){
-int A , B ;
-char operation , q;
-printf("entre le nombre A :  ");
-scanf("%d", &A );
-printf("entre l'operation : ");
-scanf("%c", &operation );
-printf("entre le nombre B :  ");
-scanf("%d", &B );
 
 switch (operation){
 
